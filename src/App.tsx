6ee0d8d5201e826a04f5624750d7557b187e7f57
MyTinniDev <team@mytinni.com>
2026-04-07 16:17:13 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Index from './pages/Index'
 import PrivacyPolicy from './pages/PrivacyPolicy'
 import DeleteAccount from './pages/DeleteAccount'
 import NotFound from './pages/NotFound'
+import Support from './pages/Support'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/privacy" element={<PrivacyPolicy />} />
         <Route path="/delete-account" element={<DeleteAccount />} />
         <Route path="*" element={<NotFound />} />
+        <Route path="/support" element={<Support />} />
       </Routes>
     </BrowserRouter>
   )
