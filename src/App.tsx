15404b3602b10f01589a8aed93a6a455bf3fca37
MyTinniDev <257847113+MyTinniDev@users.noreply.github.com>
2026-09-01 17:36:58 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import PrivacyPolicy from './pages/PrivacyPolicy'
 import DeleteAccount from './pages/DeleteAccount'
 import NotFound from './pages/NotFound'
 import Support from './pages/Support'
+import AgeSuitability from './pages/AgeSuitability'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Index />} />
         <Route path="/privacy" element={<PrivacyPolicy />} />
         <Route path="/delete-account" element={<DeleteAccount />} />
+        <Route path="/age-suitability" element={<AgeSuitability />} />
         <Route path="*" element={<NotFound />} />
         <Route path="/support" element={<Support />} />
       </Routes>
